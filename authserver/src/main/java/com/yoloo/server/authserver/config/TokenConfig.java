a094003fd4acbeb04f26299bf05698a40b269960
Yasin Sinan Kayacan <yasinsinan707@gmail.com>
2018-01-02 23:59:23 +0300

    Move password to application.yml

--- authserver/src/main/java/com/yoloo/server/authserver/config/TokenConfig.java
+++ authserver/src/main/java/com/yoloo/server/authserver/config/TokenConfig.java
@@ -1,7 +1,9 @@
 package com.yoloo.server.authserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -11,6 +13,13 @@ import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFacto
 @Configuration
 public class TokenConfig {
 
+  private final Environment environment;
+
+  @Autowired
+  public TokenConfig(Environment environment) {
+    this.environment = environment;
+  }
+
   @Bean
   public TokenStore tokenStore() {
     return new JwtTokenStore(jwtTokenConverter());
@@ -18,8 +27,9 @@ public class TokenConfig {
 
   @Bean
   protected JwtAccessTokenConverter jwtTokenConverter() {
+    String pwd = environment.getProperty("encrypt.key-store.password");
     KeyStoreKeyFactory keyStoreKeyFactory =
-        new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), "er49simpen".toCharArray());
+        new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"), pwd.toCharArray());
     JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
     converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
     return converter;
